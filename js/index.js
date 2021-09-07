b52560975d4a6d9542a019472d0b4a8d02163df7
groovad <59606905+groovad@users.noreply.github.com>
2021-09-08 06:03:08 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -7,19 +7,84 @@ const DEFINE_STAR_STYLE = [ '<span> &nbsp; </span>',
                             '<span class="yellow-star">★</span>',
                             '<span class="purple-star">★</span>' ];
 
+const DEFINT_CHARA_NAME = [ '',
+                            '艾伯李斯特','艾依查庫','古魯瓦爾多','阿貝爾','利恩','庫勒尼西','傑多','阿奇波爾多','馬庫斯','布列依斯',
+                            '雪莉','艾茵','伯恩哈德','弗雷特里西','瑪格莉特','多妮妲','史普拉多','貝琳達','羅索','艾妲',
+                            '梅倫','薩爾卡多','蕾格烈芙','里斯','米利安','沃肯','佛羅倫斯','帕茉','阿修羅','布朗寧',
+                            '瑪爾瑟斯','路德','魯卡','史塔夏','沃蘭德','C.C.','柯布','伊芙琳','布勞','凱倫貝克',
+                            '音音夢','康拉德','碧姬媞','庫恩','夏洛特','泰瑞爾','露緹亞','威廉','梅莉','古斯塔夫',
+                            '尤莉卡','林奈烏斯','娜汀','迪諾','奧蘭(棕)','奧蘭(黑白)','諾伊庫洛姆','出葉','希拉莉','克洛維斯',
+                            '艾莉絲泰莉雅','雨果','艾莉亞娜','格雷高爾','蕾塔','伊普西隆','波蕾特','尤哈尼','諾艾菈','勞爾',
+                            '潔米','瑟法斯','維若妮卡','里卡多','瑪麗妮菈','摩根','茱蒂絲'
+                        ];
+
 const CHARA_MIN_COST=8;
 const CHARA_MAX_COST=38;
 
+let minGap = 0;
+let charaFilterList = [];
+
+let charaFilterMenu = document.getElementById("chara-filter-menu");
+let charaList = document.getElementById("chara-list");
+let banUnitList = document.getElementById("ban-unit-list");
 let $minCostSlider = document.getElementById('min-cost');
 let $maxCostSlider = document.getElementById('max-cost');
 let $minCostInputArea = document.getElementById('input-min-cost');
 let $maxCostInputArea = document.getElementById('input-max-cost');
-let minGap = 0;
 let $resultList = document.getElementById('result-list');
 let $searchBtn = document.getElementById('search-btn');
+let showCostBtn = document.getElementById('show-cost');
+let showSlotBtn = document.getElementById('show-slot');
 
 let $cardData = [];
 let $showData = [];
+
+function scrollHorizontally(e) {
+    e = window.event || e;
+    var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+    document.getElementById('ban-unit-list').scrollLeft -= (delta * 24); // Multiplied by 40
+    e.preventDefault();
+}
+
+function openCharaFilterMenu(){
+    charaFilterMenu.classList.remove("hidden");
+}
+
+function closeCharaFilterMenu(){
+    charaFilterMenu.classList.add("hidden");
+    submitCharaFilterList();
+}
+
+function resetCharaFilterCheckbox(){
+    let allCharaFilterCheckboxClass = [].slice.call(document.getElementsByName("ban"));
+    allCharaFilterCheckboxClass.forEach( function(elem) {
+        elem.checked = false;
+    });
+}
+
+function renderCharaFilterMenu(){
+    let str ='';
+    for(i=1;i<78;i++){
+        str += `<li class="chara-face">
+                    <label><input type="checkbox" name="ban" value="${i}"><img src="./img/face/ch${i.toString().padStart(2,"0")}_f.png"></label>
+                    <div class="chara-face-name">${DEFINT_CHARA_NAME[i]}</div>
+                </li>`;
+    }
+    str += `<li class="ghost-face"></li><li class="ghost-face"></li><li class="ghost-face"></li><li class="ghost-face"></li>
+            <li class="ghost-face"></li><li class="ghost-face"></li><li class="ghost-face"></li>`
+    charaList.innerHTML=str;
+}
+
+function submitCharaFilterList() {
+    let str = '';
+    charaFilterList.splice(0,charaFilterList.length);
+    let checkedBanBox = [].slice.call(document.querySelectorAll('input[name=ban]:checked'));
+    checkedBanBox.forEach( function (i) {
+        str += `<img src="img/unit/ch${i.value.padStart(2,"0")}_unit.png" title="${DEFINT_CHARA_NAME[parseInt(i.value)]}">`; 
+        charaFilterList.push(parseInt(i.value));
+    });
+    banUnitList.innerHTML = str;
+}
 
 function changeSlotColor() {
     let nowColorNum = parseInt(this.value)
@@ -39,7 +104,7 @@ function deleteLastSlotFilterList() {
 function addToSlotFilterList() {
     let count = document.querySelectorAll(".btn-change-slot").length;
     if (count < 6) {
-        let slotFilterList = document.querySelector(".slot-filter-list");
+        let slotFilterList = document.getElementById("slot-filter-list");
         let newSlotItem = document.createElement("li");
         newSlotItem.classList.add("btn-change-slot");
         newSlotItem.setAttribute("id",`slot-${count+1}`)
@@ -52,16 +117,18 @@ function addToSlotFilterList() {
 
 function slideMin(){
     if (parseInt($minCostSlider.value) - parseInt($maxCostSlider.value) >= minGap) {
-        $minCostSlider.value = parseInt($maxCostSlider.value) - minGap;
+        $maxCostSlider.value = parseInt($minCostSlider.value) - minGap;
     }
     $minCostInputArea.value = $minCostSlider.value;
+    $maxCostInputArea.value = $maxCostSlider.value;
 }
 
 function slideMax(){
     if (parseInt($maxCostSlider.value) - parseInt($minCostSlider.value) <= minGap) {
-        $maxCostSlider.value = parseInt($minCostSlider.value) - minGap;
+        $minCostSlider.value = parseInt($maxCostSlider.value) - minGap;
     }
     $maxCostInputArea.value = $maxCostSlider.value;
+    $minCostInputArea.value = $minCostSlider.value;
 }
 
 function inputMin() {
@@ -86,12 +153,38 @@ function inputMax() {
 
 $searchBtn.addEventListener('click',doSearch);
 
+function showCardCost(){
+    let allCharaCostClass = [].slice.call(document.getElementsByClassName("chara-cost"));
+    allCharaCostClass.forEach( function(elem) {
+        elem.classList.remove("hidden");
+    });
+    let allCharaSlotClass = [].slice.call(document.getElementsByClassName("chara-slot"));
+    allCharaSlotClass.forEach( function(elem) {
+        elem.classList.add("hidden");
+    });
+    showCostBtn.classList.add("on");
+    showSlotBtn.classList.remove("on");
+}
+
+function showCardSlot(){
+    let allCharaCostClass = [].slice.call(document.getElementsByClassName("chara-cost"));
+    allCharaCostClass.forEach( function(elem) {
+        elem.classList.add("hidden");
+    });
+    let allCharaSlotClass = [].slice.call(document.getElementsByClassName("chara-slot"));
+    allCharaSlotClass.forEach( function(elem) {
+        elem.classList.remove("hidden");
+    });
+    showSlotBtn.classList.add("on");
+    showCostBtn.classList.remove("on");
+}
+
 function doSearch(){
     $showData = [];
-
-    let checkedLevels = [];
+    $searchBtn.disabled =true;
+    let levelFilterList = [];
     let checkedLevelBox = [].slice.call(document.querySelectorAll('input[name=level]:checked'))
-    checkedLevelBox.forEach( function (i) { checkedLevels.push(i.value); } );
+    checkedLevelBox.forEach( function (i) { levelFilterList.push(i.value); } );
 
     let minCostValue = parseInt($minCostSlider.value);
     let maxCostValue = parseInt($maxCostSlider.value);
@@ -101,26 +194,30 @@ function doSearch(){
     slotFilterArray.forEach( function (i) { slotFilterList.push(i.value); } );
     slotFilterList.sort();
 
+    let p_filter = 0,p_data = 0;
     $cardData.forEach( function (data) {
-        if (data.cost >=minCostValue && data.cost <=maxCostValue && checkedLevels.includes(data.level)){
-            if (slotFilterList.length == 0) {
-                $showData.push(data);
-            } else if (data.slotColor.length >= slotFilterList.length) {
-                let p_filter = 0,p_data = 0;
-                do{
-                    if (slotFilterList[p_filter] == data.slotColor[p_data]){
-                        p_filter++;
-                        p_data++;
-                    } else {
-                        p_data++;
-                    }
-                    console.log(`${p_filter},${p_data}`);
-                } while(p_filter<slotFilterList.length && p_data<data.slotColor.length);
-                if (p_filter == slotFilterList.length) { $showData.push(data); }
+        if(!charaFilterList.includes(data.charaId)) {
+            if (data.cost >=minCostValue && data.cost <=maxCostValue && levelFilterList.includes(data.level)){
+                if (slotFilterList.length == 0) {
+                    $showData.push(data);
+                } else if (data.slotColor.length >= slotFilterList.length) {
+                    p_filter = 0;
+                    p_data = 0;
+                    do{
+                        if (slotFilterList[p_filter] == data.slotColor[p_data]){
+                            p_filter++;
+                            p_data++;
+                        } else {
+                            p_data++;
+                        }
+                    } while(p_filter<slotFilterList.length && p_data<data.slotColor.length);
+                    if (p_filter == slotFilterList.length) { $showData.push(data); }
+                }
             }
         }
     });
     render($showData);
+    $searchBtn.disabled =false;
 };
 
 function render(result) {
@@ -136,11 +233,15 @@ function render(result) {
                     <p class="chara-level">${data.level}</p>
                     <img id="card-frame${data.cardId%10}" src="./img/${data.imageName}.png" alt="${data.charaName}">
                     <div class="chara-slot">${tempSlotStr}</div>
+                    <div class="chara-cost hidden">${data.cost}</div>
                 </li>
-            `;
+                `;
             tempSlotStr = '';
         }
     });
+    str += `<li class="ghost">/*</li><li class="ghost"></li><li class="ghost"></li>
+            <li class="ghost"></li><li class="ghost"></li><li class="ghost"></li>
+            <li class="ghost"></li><li class="ghost"></li><li class="ghost"></li>`;
     $resultList.innerHTML = str;
 };
 
